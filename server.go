ad598ed9f03a941d214732cc42525e42f53e1463
Collin Glass <collinglass@gmx.com>
2013-06-20 21:48:20 -0400

    Initial commit

---
+++ server.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+  "net/http"
+  "log"
+)
+
+func main() {
+  log.Println("Starting Server")
+  http.Handle("/", http.FileServer(http.Dir("./frontEnd/")))
+
+  log.Println("Listening on 8080")
+  http.ListenAndServe(":8080", nil)
+}
